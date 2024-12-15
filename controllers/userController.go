7a506b151cd39f959347420afb0536020a439128
Linabousbih <linabousbih@gmail.com>
2024-12-15 16:57:51 +0100

    All user controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -8,6 +8,7 @@ import (
 	"full-jwt/models"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -136,7 +137,60 @@ func Login() gin.HandlerFunc {
 	}
 }
 
-func GetUsers() {}
+func GetUsers() gin.HandlerFunc{
+	return func (c *gin.Context){
+		if err:=helpers.CheckUserType(c, "ADMIN"); err!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err})
+			return
+		}
+		ctx,cancel:=context.WithTimeout(context.Background(),100*time.Second)
+		defer cancel()
+
+		recordPerPage,err:=strconv.Atoi(c.Query("recordPerPage"))
+		if err!=nil || recordPerPage <1{
+			recordPerPage=10
+		}
+
+		page, err1:=strconv.Atoi(c.Query("page"))
+		if err1!=nil || page <1{
+			page=1
+		}
+
+		startIndex:=(page-1) * recordPerPage
+		startIndex,err= strconv.Atoi(c.Query("startIndex"))
+
+		matchStage:= bson.D{{"$match",bson.D{{}}}}
+
+		grouptage:= bson.D{{"$group",bson.D{
+			{"_id",bson.D{{"_id","null"}}},
+			{"total_count",bson.D{{"$sum",1}}}, 
+			{"data",bson.D{{"$push","$$ROOT"}}}
+		}}}
+
+		projectStage := bson.D{
+			{"$project", bson.D{
+				{"_id",0},
+				{"total_count", 1},
+				{"user_items", bson.D{{"$slice", []interface{}{"$data",startIndex,recordPerPage}}}},
+			}}
+		}
+
+		result,err:=userCollection.Aggregate(ctx,mongo.Pipeline{
+			matchStage, grouptage, projectStage
+		})
+
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing users"})
+		}
+
+		var allUsers []bson.M
+		if err =result.All(ctx,&allUsers); err!=nil{
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allUsers[0])
+	}
+}
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
